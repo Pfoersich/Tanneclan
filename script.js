90af3271d955c81e55928ccaf9cdc68b26eccb66
Pfoersich <melocotonero14@gmail.com>
2026-05-13 15:20:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+fetch("https://mc-link-backend-production.up.railway.app/create-code", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ name })
+});
